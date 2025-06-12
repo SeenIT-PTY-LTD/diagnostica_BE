f5a64b57bb103602adea3696f1af3bfd1a7f84a2
kunalBari5557 <kunal@humb.io>
2025-06-12 15:33:32 +0530

    prod db update

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-// const connection = mongoose.createConnection('mongodb://DiagAdmin:PainAdmin2024@54.252.90.36:27017/pain_management?directConnection=true').on('open',()=>{
-const connection = mongoose.createConnection('mongodb+srv://Admin_Diag:DiagAdmin2025@diagnostica.bufpk3m.mongodb.net/pain_management').on('open',()=>{
+const connection = mongoose.createConnection('mongodb://DiagAdmin:PainAdmin2024@54.252.90.36:27017/pain_management?directConnection=true').on('open',()=>{
+// const connection = mongoose.createConnection('mongodb+srv://Admin_Diag:DiagAdmin2025@diagnostica.bufpk3m.mongodb.net/pain_management').on('open',()=>{
     console.log("Mongo DB Connected");
 }).on('error',()=>{
     console.log("Mongo DB Connection Error")
